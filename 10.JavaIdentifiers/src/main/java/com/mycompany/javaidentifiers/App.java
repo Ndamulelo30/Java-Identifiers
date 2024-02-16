9a6e5b05a65e0f9b168ae230519e0c1c29ef1cf1
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 14:37:33 +0200

    Add files via upload

---
+++ 10.JavaIdentifiers/src/main/java/com/mycompany/javaidentifiers/App.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaidentifiers;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+      //Good
+      int  minutesPerHour = 60;
+      
+      //OK, but not so easy to understand what m actually is
+      int m = 60;
+      System.out.println(minutesPerHour);
+      System.out.println(m);
+    }
+}
